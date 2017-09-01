b0521113230d6a5df3ad6851099d5004372a2435
mamingzhang <mamingzhang@shuzijiayuan.com>
2017-09-01 15:35:31 +0800

    Add SecondBundle

--- app/src/main/java/com/horsege/atlastjavatest/MainActivity.java
+++ app/src/main/java/com/horsege/atlastjavatest/MainActivity.java
@@ -1,25 +1,63 @@
 package com.horsege.atlastjavatest;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.taobao.atlas.runtime.RuntimeVariables;
+import android.view.MenuItem;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.horsege.middleawaylibrary.BaseActivity;
 import com.taobao.android.ActivityGroupDelegate;
 
-import static android.R.attr.key;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawerLayout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        ((BottomNavigationView)findViewById(R.id.bottomNavView)).setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigationView);
+        navigationView.setNavigationItemSelectedListener(this);
+
         ActivityGroupDelegate actGroupCompat = new ActivityGroupDelegate(this, savedInstanceState);
 
         Intent intent = new Intent();
-        intent.setClassName(getBaseContext(), "com.horsege.firstbundle.FirstBundleActivity");
+        intent.setClassName(getBaseContext(), "com.horsege.secondbundle.SecondBundleActivity");
         actGroupCompat.startChildActivity((ViewGroup) findViewById(R.id.content), "FirstBundle", intent);
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+            }
+            return false;
+        }
+
+    };
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
     }
 }
---
+++ secondbundle/src/main/java/com/horsege/secondbundle/SecondBundleActivity.java
@@ -0,0 +1,14 @@
+package com.horsege.secondbundle;
+
+import android.os.Bundle;
+
+import com.horsege.middleawaylibrary.BaseActivity;
+
+public class SecondBundleActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second_bundle);
+    }
+}
